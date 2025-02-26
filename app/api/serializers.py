5d4317e1cebe19d4e5c5b586a5c4ac904dfa5fa2
Peter personal <topeter44msg@gmail.com>
2025-02-26 13:25:58 +0530

    remove updated from response

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -4,17 +4,17 @@ from app.models import Article, Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'slug', 'created_at']
         read_only_fields = ['slug']
 
 class ArticleSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='category.name', read_only=True)
+    category = CategorySerializer(read_only=True)
     
     class Meta:
         model = Article
         fields = [
             'id', 'title', 'slug', 'content', 'is_published',
-            'cover_image', 'author', 'category', 'category_name',
-            'location', 'created_at', 'updated_at'
+            'cover_image', 'author', 'category',
+            'location', 'created_at'
         ]
         read_only_fields = ['slug']
