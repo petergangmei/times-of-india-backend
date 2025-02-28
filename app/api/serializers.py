5500255314e0f35b307cdd24524e3fd8d39d2954
Peter Gangmei <topeter44msg@gmail.com>
2025-02-28 13:00:36 +0530

    update url path for article detail

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -9,12 +9,20 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
+    cover_image_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Article
         fields = [
             'id', 'title', 'slug', 'content', 'is_published',
-            'cover_image', 'author', 'category',
+            'cover_image', 'cover_image_url', 'author', 'category',
             'location', 'created_at'
         ]
         read_only_fields = ['slug']
+        
+    def get_cover_image_url(self, obj):
+        if obj.cover_image:
+            request = self.context.get('request')
+            if request is not None:
+                return request.build_absolute_uri(obj.cover_image.url)
+        return None
